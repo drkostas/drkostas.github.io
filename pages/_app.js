a2d4658609e5d00211ed90b5f9b0ec6febde439f
drkostas <georgiou.kostas94@gmail.com>
2022-10-31 14:49:31 -0400

    Added extra vercel component

--- pages/_app.js
+++ pages/_app.js
@@ -3,6 +3,7 @@ import Layout from "../components/Layout";
 import Head from "../components/Head";
 import "../styles/globals.css";
 import "../styles/themes.css";
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
 
@@ -19,6 +20,7 @@ function MyApp({ Component, pageProps }) {
     <Layout>
       <Head title={`Kostas Georgiou | ${pageProps.title}`} />
       <Component {...pageProps} />
+      <Analytics />
     </Layout>
   );
 }
