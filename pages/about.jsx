b18e29fb1a627bb180d284935d9a7319702fae7d
drkostas <georgiou.kostas94@gmail.com>
2023-02-01 14:39:46 -0500

    Update about page and Resume

--- pages/about.jsx
+++ pages/about.jsx
@@ -8,9 +8,9 @@ const AboutPage = () => {
       <h3>About Me</h3><br/>
       <ul>
         <li><span role="img" aria-label="book">📖</span> Pursuing PhD in Data Science & Engineering @ <a href='https://bredesencenter.utk.edu/the-data-science-and-engineering-phd/'>The University of Tennessee</a>.</li>
-        <li><span role="img" aria-label="graduate-hat">🎓</span> Researching on <b>AI</b> and <b>Computer Vision</b> @ <a href='https://aicip.github.io/research.htm'>AICIP Lab</a>.</li>
+        <li><span role="img" aria-label="graduate-hat">🎓</span> Conducting research on <b>AI</b> and <b>Computer Vision</b> @ <a href='https://aicip.github.io/research.htm'>AICIP Lab</a>.</li>
         <li><span role="img" aria-label="light-bulb">💡</span> Open to collaborating on projects and innovative ideas. </li>
-        <li><span role="img" aria-label="laptop">💻</span> Currently working on Semantic Segmentation and Reinforcement Learning. </li>
+        <li><span role="img" aria-label="laptop">💻</span> Currently working on Semantic Segmentation, Domain Adaptation, and Remote Sensing. </li>
       </ul>
       <br/>
       <center>
