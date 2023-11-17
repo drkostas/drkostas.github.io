738ce26f6229787a6e49533daf91397e11e87fae
drkostas <georgiou.kostas94@gmail.com>
2023-11-17 15:45:28 -0500

    Update about

--- pages/about.jsx
+++ pages/about.jsx
@@ -10,7 +10,7 @@ const AboutPage = () => {
         <li><span role="img" aria-label="book">📖</span> Pursuing PhD in Data Science & Engineering @ <a href='https://bredesencenter.utk.edu/the-data-science-and-engineering-phd/'>The University of Tennessee</a>.</li>
         <li><span role="img" aria-label="graduate-hat">🎓</span> Conducting research on <b>AI</b> and <b>Computer Vision</b> @ <a href='https://aicip.github.io/research.htm'>AICIP Lab</a>.</li>
         <li><span role="img" aria-label="light-bulb">💡</span> Open to collaborating on projects and innovative ideas. </li>
-        <li><span role="img" aria-label="laptop">💻</span> Currently working on Semantic Segmentation, Domain Adaptation, and Remote Sensing. </li>
+        <li><span role="img" aria-label="laptop">💻</span> Currently building Masked Image Modeling models for Remote Sensing data. </li>
       </ul>
       <br/>
       <center>
